e78312000971ade848f2580fce72d72c223b46db
KRXDevelops-Kaan <42238770+krxdev-kaan@users.noreply.github.com>
2020-02-04 01:20:02 +0300

    Add files via upload

---
+++ java/frc/robot/java/frc/robot/extensions/Servo.java
@@ -0,0 +1,224 @@
+package frc.robot.extensions;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Servo {
+    protected int MIN_PULSE_WIDTH = 544;
+    protected int MAX_PULSE_WIDTH = 2400;
+
+    protected float lastAngle;
+
+    protected DigitalOutput pinOut;
+
+    protected boolean shouldStopMotor = false;
+
+    public Servo(int pin) {
+        pinOut = new DigitalOutput(pin);
+        pinOut.setPWMRate(50);
+        pinOut.enablePWM(0.02);
+    }
+
+    public void setAngle(int angle) {
+        if (angle < 0) 
+        {
+            angle = 0;
+        } 
+        else if (angle > 180) 
+        {
+            angle = 180;
+        }
+
+        float anglePerc0100 = degreesToPerctange(angle);
+
+        lastAngle = angle;
+        setDutyCyclePerc(anglePerc0100);
+    }
+
+    public void setAngle(int angle, int delay) {
+        if (angle < 0) 
+        {
+            angle = 0;
+        } 
+        else if (angle > 180) 
+        {
+            angle = 180;
+        }
+
+        float delayInSecondsFromMillis = delay / Math.abs(getAngle() - angle);
+
+        if (angle > (int)getAngle()) 
+        {
+            for (int i = (int)getAngle(); i <= angle; i++) {
+                if (i < 0) 
+                {
+                    i = 0;
+                } 
+                else if (i > 180) 
+                {
+                    i = 180;
+                }
+
+                float anglePerc0100 = degreesToPerctange(i);
+                setDutyCyclePerc(anglePerc0100);
+
+                long a = (int) delayInSecondsFromMillis;
+
+                try 
+                {
+                    Thread.sleep(a);
+                } catch (InterruptedException e) {}
+            }
+        } 
+        else 
+        {
+            for (int i = (int)getAngle(); i >= angle; i--) 
+            {
+                if (i < 0) 
+                {
+                    i = 0;
+                } 
+                else if (i > 180) 
+                {
+                    i = 180;
+                }
+
+                float anglePerc0100 = degreesToPerctange(i);
+                setDutyCyclePerc(anglePerc0100);
+
+                long a = (int)delayInSecondsFromMillis;
+
+                try 
+                {
+                    Thread.sleep(a);
+                } catch (InterruptedException e) {}
+            }
+        }
+
+        lastAngle = angle;
+    }
+
+    public float getAngle() 
+    {
+        return lastAngle;
+    }
+
+    public void steerRight(int speed) 
+    {
+        float baseSeconds = 10;
+        float processedSeconds = baseSeconds / speed;
+        float processedMillisF = processedSeconds;
+        int processedMillis = (int)processedMillisF;
+
+        for (int i = (int)getAngle(); i <= 180; i++) 
+        {
+            if (i < 0) 
+            {
+                i = 0;
+            } 
+            else if (i > 180) 
+            {
+                i = 180;
+            }
+
+            if (shouldStopMotor) 
+            {
+                shouldStopMotor = false;
+                break;
+            }
+
+            float anglePerc0100 = degreesToPerctange(i);
+            setDutyCyclePerc(anglePerc0100);
+
+            long a = (int)processedMillis;
+
+            try 
+            {
+                Thread.sleep(a);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    public void steerLeft(int speed) 
+    {
+        float baseSeconds = 10;
+        float processedSeconds = baseSeconds / speed;
+        float processedMillisF = processedSeconds;
+        int processedMillis = (int)processedMillisF;
+
+        for (int i = (int)getAngle(); i >= 0; i--) 
+        {
+            if (i < 0) 
+            {
+                i = 0;
+            } 
+            else if (i > 180) 
+            {
+                i = 180;
+            }
+
+            if (shouldStopMotor) 
+            {
+                shouldStopMotor = false;
+                break;
+            }
+
+            float anglePerc0100 = degreesToPerctange(i);
+            setDutyCyclePerc(anglePerc0100);
+
+            long a = (int)processedMillis;
+
+            try 
+            {
+                Thread.sleep(a);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    public void stopMotor() 
+    {
+        shouldStopMotor = true;
+    }
+
+    protected float degreesToPerctange(float degree) 
+    {
+        float binaryRangedPerc = degree / 180; //exceptional binary range
+        float perc0100 = binaryRangedPerc * 100;
+
+        return perc0100;
+    }
+
+    protected float convertToBinaryRange(float perc) 
+    {
+        float result = perc / 100;
+        
+        return result;
+    }
+
+    protected float convertToServoFrequencyRange(float perc) 
+    {
+        float SERVO_MIN_FREQ = 0.02f;
+
+        float binaryRangedPerc = convertToBinaryRange(perc);
+        float servoRangedPerc010 = binaryRangedPerc / 10;
+        float servoRangedPerc212 = servoRangedPerc010 + SERVO_MIN_FREQ;
+
+        return servoRangedPerc212;
+    }
+    
+    protected void setDutyCyclePerc(float perc) 
+    {
+        float servoFreqRangedPerc = convertToServoFrequencyRange(perc);
+
+        pinOut.updateDutyCycle(servoFreqRangedPerc);
+    }
+
+    protected void manuelDutyCycleUpdate(float p) 
+    {
+        pinOut.updateDutyCycle(p);
+    }
+}
